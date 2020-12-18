4a543b6478031c2c2ccbe9ee1058993c68fa6196
Caleb Trachte <cetrachte@hotmail.com>
2020-12-18 11:18:04 -0600

    add origin/destination xhr reqs, cleanup

--- helpers/WeatherStack.js
+++ helpers/WeatherStack.js
@@ -1,19 +1,33 @@
-
-
+// triggers calls to Weatherstack API on click of 'submit' button
 document.getElementById("findFlights").addEventListener('click', function () {
-    const originLocation = document.getElementById("Origin").value;
-    const destLocation = document.getElementById("Destination").value;
+
     // origin location weather call
+    const originLocation = document.getElementById("Origin").value || "New York, NY";
     const xhrOriginWeatherStack = new XMLHttpRequest();
     let queryStringOrigin = "http://api.weatherstack.com/current"
         + "? access_key = " + WeatherstackKey
         + "& query = " + originLocation;
-    xhrOriginWeatherStack.withCredentials = true;
+    // xhrOriginWeatherStack.withCredentials = true;
     xhrOriginWeatherStack.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
-            console.log(this.responseText);
+            console.log("Origin weatherstack res:"this.responseText);
         }
     });
     xhrOriginWeatherStack.open("GET", queryStringOrigin);
     xhrOriginWeatherStack.send(data);
+
+    // destination location weather call
+    const destinationLocation = document.getElementById("Destination").value || "Portland, OR";
+    const xhrDestinationWeatherStack = new XMLHttpRequest();
+    let queryStringDestination = "http://api.weatherstack.com/current"
+        + "? access_key = " + WeatherstackKey
+        + "& query = " + destinationLocation;
+        // xhrDestinationWeatherStack.withCredentials = true;
+        xhrDestinationWeatherStack.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log("Destination weatherstack res:", this.responseText);
+        }
+    });
+    xhrDestinationWeatherStack.open("GET", queryStringDestination);
+    xhrDestinationWeatherStack.send(data);
 });
