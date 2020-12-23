66393f35b79baaf66a2630110a739c85f3400317
Caleb Trachte <cetrachte@hotmail.com>
2020-12-23 09:10:21 -0600

    add all necessary data points to html UI, connected to API calls now.

--- helpers/WeatherStack.js
+++ helpers/WeatherStack.js
@@ -3,7 +3,7 @@ document.getElementById("findFlights").addEventListener('click', function () {
     // origin location weather call
     const data = null;
     // origin location weather call, or a random index from the airportData
-    const originLocation = originData || airportData[ Math.floor(Math.random() * Math.floor(300))];
+    const originLocation = originData || airportData[Math.floor(Math.random() * Math.floor(300))];
     const xhrOriginWeatherStack = new XMLHttpRequest();
     let queryStringOrigin = "http://api.weatherstack.com/current" + "?access_key=" + WeatherstackKey + "&query=" + originLocation.city + ", " + originLocation.state;
     // xhrOriginWeatherStack.withCredentials = true;
@@ -11,7 +11,10 @@ document.getElementById("findFlights").addEventListener('click', function () {
         if (this.readyState === this.DONE) {
             let response = JSON.parse(this.responseText);
             console.log("Origin weatherstack res:", response);
-            document.getElementById('originWeatherIcon').setAttribute('src', response.current.weather_icons);
+            document.getElementById('originWeatherIcon').setAttribute('src', response.current.weather_icons[0]);
+            document.getElementById('originWeatherDesc').innerHTML = response.current.weather_descriptions[0];
+            document.getElementById('originWeatherTemp').innerHTML = response.current.temperature + "C";
+            document.getElementById('originWeatherCity').innerHTML = response.location.name + ", " + response.location.region;
         }
     });
     xhrOriginWeatherStack.open("GET", queryStringOrigin);
@@ -19,15 +22,18 @@ document.getElementById("findFlights").addEventListener('click', function () {
     xhrOriginWeatherStack.send(data);
 
     // destination location weather call, or a random index from the airportData
-    const destinationLocation = destinationData || airportData[ Math.floor(Math.random() * Math.floor(300))];
+    const destinationLocation = destinationData || airportData[Math.floor(Math.random() * Math.floor(300))];
     const xhrDestinationWeatherStack = new XMLHttpRequest();
     let queryStringDestination = "http://api.weatherstack.com/current" + "?access_key=" + WeatherstackKey + "&query=" + destinationLocation.city + ", " + destinationLocation.state;
-        // xhrDestinationWeatherStack.withCredentials = true;
-        xhrDestinationWeatherStack.addEventListener("readystatechange", function () {
+    // xhrDestinationWeatherStack.withCredentials = true;
+    xhrDestinationWeatherStack.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
             let response = JSON.parse(this.responseText);
             console.log("Destination weatherstack res:", response);
-            document.getElementById('destinationWeatherIcon').setAttribute('src', response.current.weather_icons);
+            document.getElementById('destWeatherIcon').setAttribute('src', response.current.weather_icons[0]);
+            document.getElementById('destWeatherDesc').innerHTML = response.current.weather_descriptions[0];
+            document.getElementById('destWeatherTemp').innerHTML = response.current.temperature + "C";
+            document.getElementById('destWeatherCity').innerHTML = response.location.name + ", " + response.location.region;
         }
     });
     xhrDestinationWeatherStack.open("GET", queryStringDestination);
