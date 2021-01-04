1c7a86254fa48fe12f9f6fa44efde6670b5a223a
Caleb Trachte <cetrachte@hotmail.com>
2021-01-04 13:53:15 -0600

    corrected errors with query params

--- helpers/SkyScanner.js
+++ helpers/SkyScanner.js
@@ -3,10 +3,10 @@ document.getElementById("findFlights").addEventListener('click', function () {
     // origin location call
     const data = null;
     // origin location call
-    const originAirportCode = originData || airportData[Math.floor(Math.random() * Math.floor(300))];
-    const destAirportCode = (destinationData || airportData[Math.floor(Math.random() * Math.floor(300))]).code;
+    const originAirportCode = (originData || airportData[Math.floor(Math.random() * Math.floor(300))]).code + "-sky";
+    const destAirportCode = (destinationData || airportData[Math.floor(Math.random() * Math.floor(300))]).code  + "-sky";
     const inboundpartialdate = document.querySelectorAll('[type="date"]')[0].value; // modify this later to be more specific to the HTML element.
-
+    console.log(originAirportCode, destAirportCode, inboundpartialdate);
     const xhrSkyScannerRequest = new XMLHttpRequest();
     const queryString = "/apiservices/browsedates/v1.0/US/USD/en-US/" + originAirportCode + "/" + destAirportCode + "/" + inboundpartialdate + "?inboundpartialdate=" + inboundpartialdate;
     xhrSkyScannerRequest.withCredentials = true;
