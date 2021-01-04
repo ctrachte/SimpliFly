bb022a6f5685ca4f8272768420736af41df246f6
Caleb Trachte <cetrachte@hotmail.com>
2021-01-04 14:17:27 -0600

    fixed date formatting issues

--- helpers/SkyScanner.js
+++ helpers/SkyScanner.js
@@ -1,9 +1,15 @@
+// we need to formate dates properly for the API, so lets extend the DATE class
+Date.prototype.formatSkyScanner = function(){
+    return  this.getFullYear() + "-"
+    + (this.getMonth().length > 1 ? (this.getMonth() + 1) : "0" + (this.getMonth() + 1)) +
+    "-" +  (this.getDate().length > 1 ? (this.getDate() + 1) : "0" + (this.getDate() + 1));
+}
+
 // triggers calls to SkyScanner API on click of 'submit' button
 document.getElementById("findFlights").addEventListener('click', function () {
-    const data = null;
     const originAirportCode = (originData || airportData[Math.floor(Math.random() * Math.floor(300))]).code + "-sky";
     const destAirportCode = (destinationData || airportData[Math.floor(Math.random() * Math.floor(300))]).code  + "-sky";
-    const inboundpartialdate = document.getElementById('inbound-partial-date').value; // modify this later to be more specific to the HTML element.
+    const inboundpartialdate = (document.getElementById('inbound-partial-date').value) || new Date().formatSkyScanner(); // modify this later to be more specific to the HTML element.
     // console.log(originAirportCode, destAirportCode, inboundpartialdate);
     const xhrSkyScannerRequest = new XMLHttpRequest();
     const queryString = "/apiservices/browsedates/v1.0/US/USD/en-US/" + originAirportCode + "/" + destAirportCode + "/" + inboundpartialdate + "?inboundpartialdate=" + inboundpartialdate;
@@ -18,5 +24,5 @@ document.getElementById("findFlights").addEventListener('click', function () {
     xhrSkyScannerRequest.open("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com" + queryString);
     xhrSkyScannerRequest.setRequestHeader("x-rapidapi-key", SkyScannerKey);
     xhrSkyScannerRequest.setRequestHeader("x-rapidapi-host", SkyScannerHost);
-    xhrSkyScannerRequest.send(data);
+    xhrSkyScannerRequest.send();
 });
