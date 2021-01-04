99760dde58bc99d8f39aaade20b00f76b0359cff
Caleb Trachte <cetrachte@hotmail.com>
2021-01-04 14:04:56 -0600

    add id to date input, adjust api call

--- helpers/SkyScanner.js
+++ helpers/SkyScanner.js
@@ -3,15 +3,16 @@ document.getElementById("findFlights").addEventListener('click', function () {
     const data = null;
     const originAirportCode = (originData || airportData[Math.floor(Math.random() * Math.floor(300))]).code + "-sky";
     const destAirportCode = (destinationData || airportData[Math.floor(Math.random() * Math.floor(300))]).code  + "-sky";
-    const inboundpartialdate = document.querySelectorAll('[type="date"]')[0].value; // modify this later to be more specific to the HTML element.
-    console.log(originAirportCode, destAirportCode, inboundpartialdate);
+    const inboundpartialdate = document.getElementById('inbound-partial-date').value; // modify this later to be more specific to the HTML element.
+    // console.log(originAirportCode, destAirportCode, inboundpartialdate);
     const xhrSkyScannerRequest = new XMLHttpRequest();
     const queryString = "/apiservices/browsedates/v1.0/US/USD/en-US/" + originAirportCode + "/" + destAirportCode + "/" + inboundpartialdate + "?inboundpartialdate=" + inboundpartialdate;
     xhrSkyScannerRequest.withCredentials = true;
 
     xhrSkyScannerRequest.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
-            console.log(this.responseText);
+            // TODO: map data returned to the UI, and handle errors
+            console.log(JSON.parse(this.responseText));
         }
     });
     xhrSkyScannerRequest.open("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com" + queryString);
