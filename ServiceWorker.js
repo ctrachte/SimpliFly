2120d3046cd01ee965ef0125a723217472c52a4b
Caleb Trachte <cetrachte@hotmail.com>
2021-01-08 12:17:15 -0600

    commented out broken code for merge

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -14,43 +14,43 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        // './helpers/',
-      ]);
-    })
-  );
-});
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open('v1').then((cache) => {
+//       return cache.addAll([
+//         './',
+//         // './helpers/',
+//       ]);
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-  );
-});
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request)
+//   );
+// });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request).then(function(response) {
-    // caches.match() always resolves
-    // but in case of success response will have value
-    if (response !== undefined) {
-      return response;
-    } else {
-      return fetch(event.request).then(function (response) {
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        let responseClone = response.clone();
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(caches.match(event.request).then(function(response) {
+//     // caches.match() always resolves
+//     // but in case of success response will have value
+//     if (response !== undefined) {
+//       return response;
+//     } else {
+//       return fetch(event.request).then(function (response) {
+//         // response may be used only once
+//         // we need to save clone to put one copy in cache
+//         // and serve second one
+//         let responseClone = response.clone();
         
-        caches.open('v1').then(function (cache) {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      }).catch(function () {
-        return caches.match(event.request);
-      });
-    }
-  }));
-});
+//         caches.open('v1').then(function (cache) {
+//           cache.put(event.request, responseClone);
+//         });
+//         return response;
+//       }).catch(function () {
+//         return caches.match(event.request);
+//       });
+//     }
+//   }));
+// });
